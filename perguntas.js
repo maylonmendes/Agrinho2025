9b749e86010ca7a47202a888bc694947f2bc5945
maylonmendes <141340388+maylonmendes@users.noreply.github.com>
2025-06-13 07:39:08 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,36 +1,36 @@
 criaCartao(
     'quizz',
-    'Qual a escala de trabalho do Neymar?',
-    '364x1 e em ano bisexto: 365x1'
+    'Qual é a capital do estado do Paraná?',
+    'Curitiba'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o animal mais comum do estado do Paraná?',
+    'Capivara'
 )
 
 criaCartao(
     'Programação',
-    'O que é Criptomoeda?',
-    'Uma moeda virtual'
+    'Quais são as Cataratas do Iguaçu e onde elas estão localizadas no Paraná?',
+    'As Cataratas do Iguaçu são um conjunto de cerca de 275 quedas de água, está localizada na fronteira entre Brasil e Argentina em Foz do Iguaçu.'
 )
 
 criaCartao(
     'Quizz',
-    'O Pelé tem quantas copas do mundo?',
-    'Ele tem 3'
+    'Qual é a importância histórica da cidade de Paranaguá?',
+    'A cidade de Paranaguá tem uma importância histórica imensa para o desenvolvimento do Paraná por ser o primeiro porto do estado e uma das cidades mais antigas.'
 )
 
 criaCartao(
     'Quizz Santos FC.',
-    'Quantas libertadores o Santos FC tem?',
-    '3 libertadores'
+    'Além da agricultura, qual outra atividade econômica é muito relevante para o Paraná?',
+    'Além da agricultura, outra atividade econômica muito relevante para o Paraná é a indústria, especialmente a de transformação.'
 )
 
 
 criaCartao(
     'Programação',
-    'Qual a finalidade da variável em javascript?',
-    'Armazenar valores aleatórios a serem transcritos no html'
+    'Qual é o nome da serra que corta o litoral paranaense e é conhecida por suas belezas naturais e pela ferrovia histórica?',
+    'O nome da serra que corta o litoral paranaense e é conhecida por suas belezas naturais e pela ferrovia histórica é a Serra do Mar. A famosa ferrovia que liga Curitiba a Morretes e Paranaguá.'
 )
