2b509f48707e6757af4ad26f02180424dda11e84
maylonmendes <141340388+maylonmendes@users.noreply.github.com>
2025-06-13 07:44:10 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,5 @@
 criaCartao(
-    'quizz',
+    'Geografia',
     'Qual é a capital do estado do Paraná?',
     'Curitiba'
 )
@@ -11,26 +11,26 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
+    'Geografia',
     'Quais são as Cataratas do Iguaçu e onde elas estão localizadas no Paraná?',
     'As Cataratas do Iguaçu são um conjunto de cerca de 275 quedas de água, está localizada na fronteira entre Brasil e Argentina em Foz do Iguaçu.'
 )
 
 criaCartao(
-    'Quizz',
+    'Geografia',
     'Qual é a importância histórica da cidade de Paranaguá?',
     'A cidade de Paranaguá tem uma importância histórica imensa para o desenvolvimento do Paraná por ser o primeiro porto do estado e uma das cidades mais antigas.'
 )
 
 criaCartao(
-    'Quizz Santos FC.',
+    'Geografia',
     'Além da agricultura, qual outra atividade econômica é muito relevante para o Paraná?',
     'Além da agricultura, outra atividade econômica muito relevante para o Paraná é a indústria, especialmente a de transformação.'
 )
 
 
 criaCartao(
-    'Programação',
+    'Geografia',
     'Qual é o nome da serra que corta o litoral paranaense e é conhecida por suas belezas naturais e pela ferrovia histórica?',
     'O nome da serra que corta o litoral paranaense e é conhecida por suas belezas naturais e pela ferrovia histórica é a Serra do Mar. A famosa ferrovia que liga Curitiba a Morretes e Paranaguá.'
 )
